6337cd4e049162d320f13c836c84e0e2d8cd11e4
amirmirjalili <mirjalili16@gmail.com>
2024-04-07 23:34:49 +0330

    initialize

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
+@Module({
+  imports: [MongooseModule.forRoot(process.env.DB)],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
